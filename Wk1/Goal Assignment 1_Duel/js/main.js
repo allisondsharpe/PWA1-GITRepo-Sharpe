3d0004dc89e7ba27b91af07749e05f542db467c4
allison sharpe <allisonsharpe@ymail.com>
2014-11-29 17:16:01 -0500

    Homework #1: Commit #1
    Name, date, assignment, and comments

---
+++ Wk1/Goal Assignment 1_Duel/js/main.js
@@ -0,0 +1,19 @@
+/**
+ * Name: Allison Sharpe //My name
+ * Date: 11-26-14 //The date I began working on this assignment
+
+ Assignment 1 //The assignment number
+ Part 1/3 of series //The first part of the series
+*/
+
+
+
+
+
+
+
+
+
+
+
+
