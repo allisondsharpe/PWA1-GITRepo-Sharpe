0d34d0909665373e23cfc898cd3e65c5f1b02239
allison sharpe <allisonsharpe@ymail.com>
2014-11-29 17:40:10 -0500

    Homework #1: Commit #3
    Variables: Player names, damage, and health

--- Wk1/Goal Assignment 1_Duel/js/main.js
+++ Wk1/Goal Assignment 1_Duel/js/main.js
@@ -10,3 +10,21 @@
 (function(){
 
  console.log("FIGHT!!!"); //Displays the message "FIGHT!!!" on the console
+
+  //Player name
+  var playerOneName = "Jason Brody"; //The name of player one
+  var playerTwoName = "Vaas Montenegro"; //The name of player two
+
+  //Player damage
+  var player1Damage = 20; //The total amount of damage that will be inflicted upon player one
+  var player2Damage = 20; //The total amount of damage that will be inflicted upon player two
+
+  //Player health
+  var playerOneHealth = 100; //The total amount of health that player one starts off with
+  var playerTwoHealth = 100; //The total amount of health that player two starts off with
+
+  //Initiates round
+  var round = 0; //The variable for each round
+
+
+
