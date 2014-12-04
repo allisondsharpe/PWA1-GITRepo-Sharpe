85e481caf0dc7a7521b60b80dec30c93ff4fb7fc
allison sharpe <allisonsharpe@ymail.com>
2014-12-04 15:25:28 -0500

    JS Practice v2
    1: Average of an array of numbers

--- goal2_assign_JSpractice/js/main.js
+++ goal2_assign_JSpractice/js/main.js
@@ -5,7 +5,17 @@ Assignment: JS Practice v2
  */
 
 // self-executing function
-(function(){
+(function() {
 
+    console.log('------ Goal2: Assignment: JavaScript Practice ----------');
 
-  
+    console.log("1. avg of an array of numbers");
+
+    function avgNumbers(avgNum) {
+        var avgNumbers = avgNum[0] + avgNum[1] + avgNum[2] + avgNum[3] + avgNum[4];
+
+        console.log('avg number = ', avgNumbers([1, 2, 3, 4, 5]));
+        return avgNumbers;
+    }
+
+ 
