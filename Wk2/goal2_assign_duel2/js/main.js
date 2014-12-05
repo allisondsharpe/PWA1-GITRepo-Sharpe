3f55a29890b8f0ca70476858fd4bd394773617d1
allison sharpe <allisonsharpe@ymail.com>
2014-12-05 16:53:19 -0500

    Assignment: Duel v2
    For loop and variable for math formulas

--- Wk2/goal2_assign_duel2/js/main.js
+++ Wk2/goal2_assign_duel2/js/main.js
@@ -20,6 +20,19 @@
         alert(playerOne[0]+":"+playerOne[2]+" *START* "+playerTwo[0]+":"+playerTwo[2]); //Alerts the user that the fight has started
 
 
+        for(var i=0; i<10; i++) { //"The "for loop" for the variables
+            var minDamage1 = playerOne[1] * .5; //Variable for player one's minimum damage amount
+            var minDamage2 = playerTwo[1] * .5; //Variable for player two's minimum damage amount
+            var f1 = Math.floor(Math.random() * (playerOne[1] - minDamage1) + minDamage1); //The Math.floor and Math.random formulas for player one
+            var f2 = Math.floor(Math.random() * (playerTwo[1] - minDamage2) + minDamage2); //The Math.floor and Math.random formulas for player two
+
+            playerOne[2] -= f1; //
+            playerTwo[2] -= f2; //
+            //Closing curly brace for "for loop"
+            console.log(playerOne[2] + " " + playerTwo[2]); //Displays the players damage to the console
+
+
+
 
 
 
