1cfbf1b399cffc939b80217b7f975636eb0319da
allison sharpe <allisonsharpe@ymail.com>
2014-12-05 17:23:05 -0500

    JS Practice week #2
    Steps 1-6 completed

---
+++ goal2_assign_JSpractice/js/main.js
@@ -0,0 +1,29 @@
+/*
+Name: Allison Sharpe //My full name
+Date: 12-3-14 //Date started
+Assignment: JS Practice v2 //Assignment information
+ */
+
+(function() { //Self-executing function
+
+    console.log('------ Goal2: Assignment: JavaScript Practice ----------'); //Displays the goal for this assignment to the console
+
+    console.log("1. avg of an array of numbers"); //Displays the message "1. avg of an array of numbers" to the console
+
+    var total = [1, 2, 3, 4, 5]; //Array index variables
+
+    function avgNumbers(total) { //Executes function for the average of numbers
+        var avgNumbers = total[0] + total[1] + total[2] + total[3] + total[4]; //Arrays of average numbers
+
+        console.log("avg number = " + avgNumbers); //Displays the total average to the console
+        return avgNumbers; //Returns to function
+    } //Closing curly brace for function
+
+  
+
+
+
+
+
+
+
