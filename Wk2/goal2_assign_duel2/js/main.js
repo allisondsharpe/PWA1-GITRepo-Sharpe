7e9de33f5198186010211fcc000c568f206d6f99
allison sharpe <allisonsharpe@ymail.com>
2014-12-05 16:51:02 -0500

    Assignment: Duel v2
    Self-executing function and array variables

--- Wk2/goal2_assign_duel2/js/main.js
+++ Wk2/goal2_assign_duel2/js/main.js
@@ -6,3 +6,22 @@
  Part 2/3 of series
 */
 
+(function(){ //Executes function for code
+
+    console.log("FIGHT!!!"); //Displays the message "FIGHT!!!" on the console
+
+    var playerOne = ["Jason Brody", 20, 100]; //Array for player one
+    var playerTwo = ["Vaas Montenegro", 20, 100]; //Array for player two
+
+    var round = 1; //Variable for each round
+
+ 
+
+
+
+
+
+
+
+
+
