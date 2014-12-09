c8f0957a7304bf682d050ac9abec2efb6e217157
allison sharpe <allisonsharpe@ymail.com>
2014-12-09 16:05:27 -0500

    Duel #3 week 3
    Self-executing function and variables

--- W3/goal4_assign_duel3/js/main.js
+++ W3/goal4_assign_duel3/js/main.js
@@ -6,6 +6,16 @@
  Part 3/3 of series
 */
 
+(function(){ //Executes function for code
+
+    console.log("FIGHT!"); //Displays the message "FIGHT!!!" on the console
+
+    var playerOne_txt = document.getElementById("jb").querySelector("p"); //Variable for player one
+    var playerTwo_txt = document.getElementById("vm").querySelector("p"); //Variable for player two
+    var round_txt = document.querySelector("h5"); //Variable for the text for each round
+    var button = document.getElementById("fight_btn"); //Variable for the fight button
+
+    button.addEventListener("click", fight, false); //Sets up click event
 
 
 
