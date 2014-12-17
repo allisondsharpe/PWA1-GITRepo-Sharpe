573072dad73ee16a7fe8927ab5f7d17b3c27f17d
allison sharpe <allisonsharpe@ymail.com>
2014-12-17 17:03:57 -0500

    Analyze! Objects.Person
    Main.js: For loop and interval

--- Wk4/goal7_assign_obj_to_prototypes/js/main.js
+++ Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -11,4 +11,18 @@ Assignment: Analyze! Objects.Person
     var numOfPeople = 3; //Variable for the number of people shown each time
     var interval; //Variable that enables the interval
 
-   
+    for (var i = 0; i < numOfPeople; i++) { //The "for loop" for the "numOfPeople" variable
+        var personIndex = Math.floor(Math.random() * names.length); //Math formulas for the person index
+        var person = new Person(names[personIndex], i + 1); //Constructor for the person index
+
+        populateHTML(person.name, "r" + (i + 1) + "c1"); //Displays person's name to the web page
+        populateHTML(person.job, "r" + (i + 1) + "c2"); //Displays person's job to the web page
+
+        people.push(person); //Pushes the array for person index
+        names.splice(personIndex, 1); //Splices names for person index
+
+        interval = setInterval(runUpdate, 1000/30); //Enables the interval to change actions 30 times a second
+
+    } //Closing curly brace for "for loop"
+
+ 
